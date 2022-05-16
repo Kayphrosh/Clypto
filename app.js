249d9af77abd9bdf56a35d72be78b80645bd4088
kayphrosh <olakunbiolabode@yahoo.com>
2022-05-16 10:56:12 +0100

    Almost done

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ function fixBar() {
         topBar.style.top = '0px'
         topBar.style.left = '0px'
         topBar.style.right = '0px'
-        topBar.style.backdropFilter = 'blur(20px)';
-        topBar.style.background = 'rgba(0, 0, 0, 0.8)'
+        topBar.style.background = '#000'
         topBar.style.borderBottom = '1px solid #f2f2f250'
 
     } else {
@@ -19,3 +18,18 @@ function fixBar() {
         topBar.style.borderBottom = 'none'
     }
 }
+
+const faqToggle = document.querySelectorAll('.faq-toggle')
+
+faqToggle.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle ('faq-active')
+  })
+})
+const readmoreToggle = document.querySelectorAll('.read-more-btn')
+
+readmoreToggle.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle ('read-active')
+  })
+})
